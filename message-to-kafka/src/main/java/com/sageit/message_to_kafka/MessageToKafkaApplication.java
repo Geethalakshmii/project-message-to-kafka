e723af0dc59401c963f74ecf30b74b5a77f259b6
Geethalakshmii <geethalakshmi.kondragunta@slu.edu>
2025-04-29 23:09:05 -0500

    Spring boot project setup

---
+++ message-to-kafka/src/main/java/com/sageit/message_to_kafka/MessageToKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.sageit.message_to_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageToKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageToKafkaApplication.class, args);
+	}
+
+}
---
+++ message-to-kafka/src/test/java/com/sageit/message_to_kafka/MessageToKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sageit.message_to_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageToKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
